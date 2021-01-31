44699b06a9a92c9a58f2a7cb38efa9a32544cf5a
Elizabeth <elizabeth@ziemer.net>
2021-01-31 15:11:29 -0600

    blagh

--- tech/artpop.js
+++ tech/artpop.js
@@ -1,6 +1,6 @@
 var html = '<h3> contents </h3>';
 
-var section1 = ['sigid']
+var section1 = ['sigid', 'tamagotchi']
 for (i = 0; i<section1.length; i++){
 	html += '<div class="navlink"><a href="' + section1[i] + '.html">' + section1[i] + '</a></div>'
 }
